ea7051e7330bb7410f2a31185215668a2044ec67
Ayush <ayushagrawal000@gmail.com>
2016-09-10 16:04:12 +0530

    Completed Dynamic Widget List Page

--- public/assignment/views/widget/widget-list.controller.client.js
+++ public/assignment/views/widget/widget-list.controller.client.js
@@ -6,12 +6,25 @@
         .module("WebAppMaker")
         .controller("WidgetListController",WidgetListController);
 
-    function WidgetListController($routeParams, WidgetService) {
+    function WidgetListController($sce, $routeParams, WidgetService) {
         var vm = this;
-        vm.userId = $routeParams.userId;
+        vm.pageId = $routeParams.pageId;
+        vm.getSafeHtml = getSafeHtml;
+        vm.getSafeUrl = getSafeUrl;
+
+        function getSafeUrl(widget) {
+            var urlParts = widget.url.split("/");
+            var id = urlParts[urlParts.length-1];
+            var url = "https://www.youtube.com/embed/" + id;
+            return $sce.trustAsResourceUrl(url);
+        }
+
+        function getSafeHtml(widget) {
+            return $sce.trustAsHtml(widget.text);
+        }
 
         function init() {
-            vm.websites = WidgetService.findWebsitesForUserId(vm.userId);
+            vm.widgets = WidgetService.findWidgetsForPageId(vm.pageId);
         }
         init();
 
